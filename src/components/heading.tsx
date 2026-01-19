3c602ea7ad236e360b5a3a2424b38276a3f94e6e
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-19 16:32:41 +0530

    updated font + ui

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,6 +1,6 @@
 const Heading = ({ title }: { title: string }) => {
   return (
-    <div className="text-2xl font-bold text-zinc-100 md:text-4xl">{title}</div>
+    <div className="text-xl font-bold text-zinc-100 md:text-2xl">{title}</div>
   );
 };
 

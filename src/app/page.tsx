af8053eed0d8c6e8ba7c20d5699864294d950c71
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-19 15:26:49 +0530

    added sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import About from "@/components/about";
+import Bio from "@/components/bio";
+import Skills from "@/components/skills";
+
 export default function Home() {
   return (
-    <main>
-      saatvik madan
-    </main>
+    <div className="flex flex-col gap-y-16">
+      <Bio />
+      <About />
+      <Skills />
+    </div>
   );
 }

9ad27df72a083bb7c92b675fd84b6afc769bb801
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-19 23:44:39 +0530

    added console config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;

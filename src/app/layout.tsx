574c429ba6a6c3e77c9296be1c102155c3c70386
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-19 14:15:49 +0530

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from 'next';
-import { JetBrains_Mono } from 'next/font/google';
-import './globals.css';
-import { cn } from '@/lib/utils';
+import type { Metadata } from "next";
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const jetbrainsMono = JetBrains_Mono({
-  variable: '--font-jetbrains-mono',
-  subsets: ['latin'],
+  variable: "--font-jetbrains-mono",
+  subsets: ["latin"],
 });
 
 const metaData: Metadata = {};
@@ -16,12 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body
         className={cn(
-          'min-h-screen bg-gray-950 font-sans antialiased max-w-3xl mx-auto py-12 sm:py-24 px-6',
+          "mx-auto min-h-screen max-w-3xl bg-gray-950 px-6 py-12 font-sans antialiased sm:py-24",
           jetbrainsMono.variable,
         )}
+        suppressHydrationWarning
       >
         {children}
       </body>

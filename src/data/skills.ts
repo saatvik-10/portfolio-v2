36cf81abb74fcf76d60d22e4149bc29c9459219f
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-19 15:26:41 +0530

    skills data

---
+++ src/data/skills.ts
@@ -0,0 +1,44 @@
+export const skills = [
+    {
+        name: "Typescript"
+    },
+    {
+        name: "Rust"
+    },
+    {
+        name: "Solana"
+    },
+    {
+        name: "React"
+    },
+    {
+        name: "React Native"
+    },
+    {
+        name: "Next.js"
+    },
+    {
+        name: "Node.js"
+    },
+    {
+        name: "Elysia"
+    },
+    {
+        name: "Hono"
+    },
+    {
+        name: "Express"
+    },
+    {
+        name: "Databases"
+    },
+    {
+        name: "Orms"
+    },
+    {
+        name: "DevOps"
+    },
+    {
+        name: "AWS"
+    },
+]

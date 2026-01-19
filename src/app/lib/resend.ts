a0ca2883dff7bec7d8b9812def37bcb42dc95063
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-19 11:49:42 +0530

    resend mail api

---
+++ src/app/lib/resend.ts
@@ -0,0 +1,28 @@
+'use server';
+
+import { Resend } from 'resend';
+// import { ContactInput } from '@/validators/contact.validator';
+
+const resend = new Resend(process.env.NEXT_PUBLIC_MAIL_SECRET_KEY);
+
+export const sendEmail = async (data: any) => {
+  try {
+    const { name, email, message } = data;
+
+    const result = await resend.emails.send({
+      from: process.env.NEXT_PUBLIC_MAIL as string,
+      to: 'yello@itsaatvik.dev',
+      subject: `You have received a message from ${name}`,
+      html: `
+            <p><strong>Name:</strong> ${name}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <p><strong>Message:</strong></p>
+            <p>${message}</p>
+          `,
+    });
+
+    return { success: true, data: result };
+  } catch (error: any) {
+    return { success: false, error: error };
+  }
+};

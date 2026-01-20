7485b487829b204bd723a7e7e22e9273649bb6aa
saatvik-10 <saatvikmadan529@gmail.com>
2026-01-20 22:51:15 +0530

    updated project cards

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -7,6 +7,8 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import Image from "next/image";
+import Link from "next/link";
 
 const Projects = () => {
   return (
@@ -36,9 +38,30 @@ const Projects = () => {
 
         <div className="hidden grid-cols-3 gap-4 md:grid lg:grid">
           {Array.from({ length: 6 }).map((_, index) => (
-            <Card key={index}>
-              <CardContent className="flex w-full items-start flex-col">
-                <span className="text-3xl font-semibold">{index + 1}</span>
+            <Card className="overflow-hidden p-0" key={index}>
+              <CardContent className="flex w-full flex-col items-start border-0 border-none p-0">
+                <Image src="" alt="" className="h-36 w-full bg-black" />
+                <div className="p-3">
+                  <p className="text-sm leading-4.5 text-zinc-950">
+                    A website uptime monitoring platform to monitor your
+                    websites, APIs, and endpoints with real-time status checks,
+                    email alerts, and dashboards.
+                  </p>
+                  <div className="flex flex-row items-center justify-end gap-3 text-xs">
+                    <Link
+                      className="cursor-pointer hover:font-bold hover:underline"
+                      href=""
+                    >
+                      Demo
+                    </Link>
+                    <Link
+                      className="cursor-pointer hover:font-bold hover:underline"
+                      href=""
+                    >
+                      Souce
+                    </Link>
+                  </div>
+                </div>
               </CardContent>
             </Card>
           ))}
